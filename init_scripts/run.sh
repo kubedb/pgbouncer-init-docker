cac707e7f5577792426854a228c805a67bef4e3b
Rakibul-Hossain <rakibul.hossain@appscode.com>
2022-03-29 18:31:39 +0600

    Add tls condition
    Signed-off-by: Rakibul-Hossain <rakibul.hossain@appscode.com>

--- init_scripts/run.sh
+++ init_scripts/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -euo pipefail
+
+if [ "${SSL:-0}" = "ON" ]; then
 cp -rL /tmp/certs/* /var/run/pgbouncer/tls/serving/
 chmod 0600 /var/run/pgbouncer/tls/serving/client/*
 chmod 0664 /var/run/pgbouncer/tls/serving/server/*
+fi
 
-
-ls -la /var/run/pgbouncer/tls/serving/client/
 
