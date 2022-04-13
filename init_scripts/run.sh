e1252187603ce2b4931df384702c21359afb190e
Tamal Saha <tamal@appscode.com>
2022-04-12 17:40:56 -0700

    Make multi-arch ready
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- init_scripts/run.sh
+++ init_scripts/run.sh
@@ -3,13 +3,11 @@
 set -euo pipefail
 
 if [ "${SSL:-0}" = "ON" ]; then
-  cp -rL /tmp/certs/* /var/run/pgbouncer/tls/serving/
-  chown 70:70 /var/run/pgbouncer/tls/serving/client/*
-  chown 70:70 /var/run/pgbouncer/tls/serving/server/*
-  chown 70:70 /var/run/pgbouncer/tls/serving/metrics-exporter/*
-  chmod 0600 /var/run/pgbouncer/tls/serving/client/*
-  chmod 0600 /var/run/pgbouncer/tls/serving/server/*
-  chmod 0600 /var/run/pgbouncer/tls/serving/metrics-exporter/*
+    cp -rL /tmp/certs/* /var/run/pgbouncer/tls/serving/
+    chown 70:70 /var/run/pgbouncer/tls/serving/client/*
+    chown 70:70 /var/run/pgbouncer/tls/serving/server/*
+    chown 70:70 /var/run/pgbouncer/tls/serving/metrics-exporter/*
+    chmod 0600 /var/run/pgbouncer/tls/serving/client/*
+    chmod 0600 /var/run/pgbouncer/tls/serving/server/*
+    chmod 0600 /var/run/pgbouncer/tls/serving/metrics-exporter/*
 fi
-
-
